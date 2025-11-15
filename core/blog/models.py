55e9566f87ba7ace1bd6a1ff4ff33a0a51ac70ff
Alireza Noorahmadi <alireza.noorahmadi67@gmail.com>
2025-11-15 15:39:37 +0330

    model and admin panel implementations

---
+++ core/blog/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+
+
+
+#getting user model object
+user = get_user_model()
+
+class Post(models.Model):
+    '''
+    This is a class to define posts for blog
+    '''
+    author = models.ForeignKey(user,on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField()
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    '''
+    This is a class to define categories for blog
+    '''
+    name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name

953aedb62e99b0ee51559ae0b8538c17c03b2fd3
Alireza Noorahmadi <alireza.noorahmadi67@gmail.com>
2026-07-05 18:39:56 +0330

    celery configurations

--- core/core/settings.py
+++ core/core/settings.py
@@ -176,3 +176,7 @@ EMAIL_HOST = "smtp4dev"
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 25
+
+
+# Celery Configuration
+CELERY_BROKER_URL = config("CELERY_BROKER_URL", default="redis://redis:6379/1")
